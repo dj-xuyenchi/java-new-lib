e713ca7e36fed74a2d012dffde38aac57ad0affe
Quang Anh <89133721+dj-xuyenchi@users.noreply.github.com>
2023-03-12 00:30:24 +0700

    update for c#

--- javalib/src/main/java/BangKok/CompileCore/CompileMachine.java
+++ javalib/src/main/java/BangKok/CompileCore/CompileMachine.java
@@ -2,10 +2,11 @@ package BangKok.CompileCore;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class CompileMachine {
-    public static String compileData(String sourcePath) {
+    public static String compileAndRunJava(String sourcePath) {
         try {
             // Run Java program using "java"
             ProcessBuilder runProcess = new ProcessBuilder("java", sourcePath);
@@ -22,21 +23,50 @@ public class CompileMachine {
             // Wait for program to finish
             return result;
         } catch (IOException e) {
-            System.out.println("Lỗi đọc file");
+            System.out.println("Lỗi đọc file liên hệ Admin!");
             return null;
         }
+    }
 
+    public static String compileAndRunCSharp(String sourcePath, String fileName) {
+        try {
+            String command = "csc.exe /out:" + "\"" + sourcePath + fileName + ".exe\" " + sourcePath + fileName + ".cs";
+            Process processCompile = Runtime.getRuntime().exec(command);
+            InputStream inputStream = processCompile.getInputStream();
+            BufferedReader readerCompile = new BufferedReader(new InputStreamReader(inputStream));
+            String result = "";
+            String runLog = "";
+            while ((runLog = readerCompile.readLine()) != null) {
+                result += runLog + "\n";
+            }
+            if (result.contains("error")) {
+                return "Lỗi" + result.split("error")[1];
+            }
+            runLog = "";
+            result = "";
+            ProcessBuilder processBuilder = new ProcessBuilder(sourcePath + fileName + ".exe");
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            while ((runLog = reader.readLine()) != null) {
+                result += runLog + "\n";
+            }
+            return result;
+        } catch (IOException e) {
+            System.out.println("Lỗi đọc file liên hệ Admin!");
+            return null;
+        }
     }
 
     public static void main(String[] args) {
-        FileCreate.writeFile("D:\\",
-
-                "public class Main {\n" +
-                        "    public static void main(String[] args) {\n" +
-                        "        System.out.println(123);\n" +
-                        "    }\n" +
-                        "}", "Main1");
-        compileData("D:\\Main1.java");
+//        FileCreate.writeFile("D:\\",
+//
+//                "public class Main {\n" +
+//                        "    public static void main(String[] args) {\n" +
+//                        "        System.out.println(123);\n" +
+//                        "    }\n" +
+//                        "}", "Main1");
+//        compileData("D:\\Main1.java");
+        System.out.println(compileAndRunCSharp("D:\\myworkspace\\", "ee"));
     }
 
 }
--- javalib/src/main/java/BangKok/CompileCore/Main.java
+++ javalib/src/main/java/BangKok/CompileCore/Main.java
@@ -1,7 +1,21 @@
 package BangKok.CompileCore;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(123);
+        try {
+            String command = "csc.exe /out:\"D:\\myworkspace\\ee.exe\" D:\\myworkspace\\ee.cs"; // replace Test.cs with your file name
+            Process process = Runtime.getRuntime().exec(command);
+            InputStream inputStream = process.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            int exitCode = process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
