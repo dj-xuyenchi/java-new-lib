b42e1d49b4fe9479cb5f1693bc848c724bba2526
dj-xuyenchi <do.quanganh99zz@gmail.com>
2023-03-20 11:52:43 +0700

    update name

--- javalib/src/main/java/test.java
+++ javalib/src/main/java/test.java
@@ -1,5 +1,5 @@
 
-import HaNoi.QA.libPersonal.EmailMix;
+
 
 import java.util.Scanner;
 
@@ -12,6 +12,7 @@ public class test {
     private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+
         EmailMix e = new EmailMix("do.quanganh99zz@gmail.com", "xqwcfhvbxjomokjb", 2);
         e.sendContentToVer2("anhdqph19418@fpt.edu.vn", "â", "â");
     }
